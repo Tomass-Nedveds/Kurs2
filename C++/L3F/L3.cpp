4f3f7ae1ef62eec69334295250f2295da410e80e
Gagrio <95764058+Tomass-Nedveds@users.noreply.github.com>
2025-01-17 10:57:41 +0000

    b

--- C++/L3F/L3.cpp
+++ C++/L3F/L3.cpp
@@ -3,101 +3,124 @@
 #include <vector>
 using namespace std;
 
-class Media{
-    public:
-        string extension;
-        string title;
-        int size;
-        int age;
-        Media(){};
-        Media(string ex, string tl, int s, int a): extension(ex), title(tl), size(s), age(a){};
+class Media {
+protected:
+    string title;
+    string name;
+    string surname;
+    int age;
+    int size;
+    string extension;
+
+public:
+    Media() {}
+    Media(string nm, string snm, int ag, int sz, string ex, string ti)
+        : name(nm), surname(snm), age(ag), size(sz), extension(ex), title(ti) {}
+
+    void input() {
+        cout << "Enter title: ";
+        cin >> title;
+        cout << "Enter name: ";
+        cin >> name;
+        cout << "Enter surname: ";
+        cin >> surname;
+        cout << "Enter age: ";
+        cin >> age;
+        cout << "Enter size (MB): ";
+        cin >> size;
+        cout << "Enter extension: ";
+        cin >> extension;
+
+        display();
+    }
+
+    void display() {
+        cout << endl << "Title: " << title << ", Name: " << name << ", Surname: " << surname
+             << ", Age: " << age << ", Size: " << size << " MB, Extension: " << extension << endl << endl;
+    }
 };
 
-    class Vidio : public Media{
-        protected:
-            int lenght;
-            int resolution;
-        
-        public:
-            Vidio(){};
-            Vidio(int ln, int rs): lenght(ln), resolution(rs){};
+// Text
+class Text : public Media {
+public:
+    Text() : Media() {}
+    Text(string nm, string snm, int ag, int sz, string ex, string ti)
+        : Media(nm, snm, ag, sz, ex, ti) {}
+};
 
-    };
-        class Move : public Vidio{
+// Video
+class Video : public Media {
+protected:
+    int length;
+    int resolution;
 
-        };
+public:
+    Video() : Media(), length(0), resolution(0) {}
+    Video(string nm, string snm, int ag, int sz, string ex, string ti, int len, int res)
+        : Media(nm, snm, ag, sz, ex, ti), length(len), resolution(res) {}
 
-        class Series : public Vidio{
-
-        };
-
-    //Audio
-    class Audio : public Media{
-
-    };
-
-            class Music : public Audio{
-                protected:
-
-            };
-
-            class Recording : public Audio{
-
-            };
-
-    //Text
-    class Text : public Media{
-        
-    };
-        class Book : public Audio{
-
-        };
-
-        class Note : public Audio{
-
-        };
-
-    //Image
-    class Image : public Media{
-
-
-    };
-
-        class Photo : public Image{
-
-        };
-
-        class Meme : public Image{
-
-        };
-
-
-int main()
-{
-    string answer;
-
-    Move myMove;
-    myMove.title = "Graz";
-    myMove.size =  12;
-
-    cout << myMove.title;
-
-
-
-
-    /*cout << "1.Vidio" << endl << "2.Audio" << endl << "3.Text" << endl << "4.Image" << endl;
-    cout << endl;
-    cout << "Answer: ";
-    cin >> answer;
-    cout << "---------------" << endl;
-    if ("Vidio" == answer){
-        cout << "1.Move" << endl << "2.Series" << endl << endl;
-        cout << "Answer: ";
-        cin >> answer;
+    void input() {
+        Media::input();
+        cout << "Enter length (minutes): ";
+        cin >> length;
+        cout << "Enter resolution (e.g., 1080): ";
+        cin >> resolution;
     }
-    if ("Move" == answer){
 
+    void display() {
+        Media::display();
+        cout << "Length: " << length << " minutes, Resolution: " << resolution << "p" << endl;
     }
-    */
-return 0;
+};
+
+// Photo
+class Photo : public Media {
+public:
+    Photo() : Media() {}
+    Photo(string nm, string snm, int ag, int sz, string ex, string ti)
+        : Media(nm, snm, ag, sz, ex, ti) {}
+};
+
+int main() {
+    vector<Media*> mediaCollection;
+    int choice;
+    Media* media = nullptr;
+
+    while (true) {
+        cout << "Select media type to add:\n";
+        cout << "1. Text\n2. Video\n3. Photo\n4. Display all media\n5. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        if (choice == 1) {
+            media = new Text();
+        } 
+        if (choice == 2) {
+            media = new Video();
+        } 
+        if (choice == 3) {
+            media = new Photo();
+        } 
+        if (choice == 4) {
+            for (Media* m : mediaCollection) {
+                m->display();
+            }
+            continue; 
+        } 
+        if (choice == 5) {
+            break;
+        }
+
+        if (media) {
+            media->input();
+            mediaCollection.push_back(media);
+            media = nullptr;
+        }
+    }
+
+    for (Media* m : mediaCollection) {
+        delete m;
+    }
+
+    return 0;
 }
